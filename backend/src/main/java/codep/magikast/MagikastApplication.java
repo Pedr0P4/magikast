607d4d668b85f9c94699f6a1da4ddcf6570bb0a1
Pedro Paulo <pedropaulo062011@gmail.com>
2026-06-16 22:49:48 -0300

    :tada: Inicialização do backend

---
+++ backend/src/main/java/codep/magikast/MagikastApplication.java
@@ -0,0 +1,13 @@
+package codep.magikast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagikastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagikastApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/codep/magikast/MagikastApplicationTests.java
@@ -0,0 +1,13 @@
+package codep.magikast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagikastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
