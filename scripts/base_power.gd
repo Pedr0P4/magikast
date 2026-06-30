4c331e2870c525d73f7395b85cfe9266fbb423cc
Pedro Paulo <pedropaulo062011@gmail.com>
2026-06-29 21:56:13 -0300

    :construction: feat(game): Continuação de correção de bug de tiro

--- scripts/base_power.gd
+++ scripts/base_power.gd
@@ -1,33 +1,38 @@
 class_name BasePower
 extends Area2D
 
-@export var explosion_animation: AnimatedSprite2D;
-
 @onready var sprite: Sprite2D = $Sprite;
+@onready var explosion: AnimatedSprite2D = $Explosion;
+@onready var collision: CollisionShape2D = $Collision;
 
 var creator_id: int;
 var data: PowerData;
 
 func _ready() -> void:
-	if explosion_animation:
-		explosion_animation.animation_finished.connect(_on_explosion_finished);
-	else:
-		print("ERRO: AnimatedSprite2D de explosão não está atribuído no Inspetor!");
+	print("_ready do base");
+	monitoring = false
+	
+	if explosion and not explosion.animation_finished.get_connections():
+		explosion.animation_finished.connect(_on_explosion_finished)
+	
+	await get_tree().physics_frame
+	
+	monitoring = true
 
 func _physics_process(delta: float) -> void:
+	if not data: return;
 	if(data.is_attack):
 		global_position += transform.x * data.speed * delta;
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == str(creator_id): return;
-	
 	set_physics_process(false);
 	
 	sprite.visible = false;
-	if explosion_animation:
-		explosion_animation.visible = true;
-		explosion_animation.play();
-		print("Começou!");
+	if explosion:
+		print("ATIVOU A EXPLOSAO");
+		explosion.visible = true;
+		explosion.play();
 	
 	set_deferred("monitoring", false);
 	
@@ -36,7 +41,5 @@ func _on_body_entered(body: Node2D) -> void:
 	print(body.player_name + " Tomou");
 
 func _on_explosion_finished() -> void:
-	print("Terminou!");
 	if multiplayer.is_server():
 		queue_free();
-		print("ACABOU A ANIMAÇÃO");
--- scripts/powers/fireball.gd
+++ scripts/powers/fireball.gd
@@ -1,5 +1,6 @@
 extends BasePower
 
 func _ready() -> void:
+	print("_ready do fireball");
 	if not data:
 		data = load("res://resources/fireball.tres");
