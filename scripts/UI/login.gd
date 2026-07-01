cce7429a8d548353446eba384c86443015dae38d
Pedro Paulo <pedropaulo062011@gmail.com>
2026-07-01 04:23:26 -0300

    :construction: feat(game): Início da implementação de registro e login

---
+++ scripts/UI/login.gd
@@ -0,0 +1,5 @@
+extends Control
+
+@onready var username_input: LineEdit = $MarginContainer/Panel/MarginContainer/VBoxContainer/VBoxContainer/UsernameInput;
+@onready var password_input: LineEdit = $MarginContainer/Panel/MarginContainer/VBoxContainer/VBoxContainer2/PasswordInput;
+@onready var login_button: Button = $MarginContainer/Panel/MarginContainer/VBoxContainer/Button;
---
+++ scripts/UI/register.gd
@@ -0,0 +1,6 @@
+extends Control
+
+@onready var username_input: LineEdit = $MarginContainer/Panel/MarginContainer/VBoxContainer/VBoxContainer/UsernameInput;
+@onready var display_name_input: LineEdit = $MarginContainer/Panel/MarginContainer/VBoxContainer/VBoxContainer2/DisplayNameInput;
+@onready var password_input: LineEdit = $MarginContainer/Panel/MarginContainer/VBoxContainer/VBoxContainer3/PasswordInput;
+@onready var register_button: Button = $MarginContainer/Panel/MarginContainer/VBoxContainer/Button;
